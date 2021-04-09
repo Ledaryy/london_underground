c742b639e720541e97ec8f1437de35a9dc9c0650
Ledary <andrejkulishov@gmail.com>
2021-04-09 22:21:37 +0100

    map added

--- underground/assets/map.js
+++ underground/assets/map.js
@@ -1,14 +1,15 @@
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = 'Hello webpack';
-    return element;
-  }
-  document.body.appendChild(component());
-
-  import React from 'react';
+import React from 'react';
 import ReactDOM from "react-dom";
 
+import App from './App.js'
+
 ReactDOM.render(
-  <h1>Hello, react!</h1>,
+  <h1>Hello, world!</h1>,
   document.getElementById('root')
+);
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('leaflet-container')
 );
