16bde7439a48d4fa0b59930a514ef85b801d1ea5
Ledary <andrejkulishov@gmail.com>
2021-05-03 00:58:16 +0100

    config remowal

--- backend/underground/underground/settings.py
+++ backend/underground/underground/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '()*f43!nl$ku5w_^!7c0fj$ld40n31z13+m=4i-2e5b-ezqtgm'
+SECRET_KEY = '()*f43!nl$ku5w_^!7c0fj$ld40n31z13+m=4i-2c5b-ezqtgm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
