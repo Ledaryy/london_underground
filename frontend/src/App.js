8ea95dbd179665f08af4ea8a0c24b09d9540bb1a
Ledary <andrejkulishov@gmail.com>
2021-05-05 20:40:53 +0100

    navigation bar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import React from 'react';
+import Navbar from "./components/Navbar/Navbar"
+
+import Map from './components/map'
 
 function App() {
   return (
-    null
+    <Navbar></Navbar>
   )
 }
 
+// map module
+Map()
 
 export default App;
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './App';
-import Map from './components/map'
+
 
 import reportWebVitals from './reportWebVitals';
 import { Breadcrumb } from 'antd';
@@ -11,23 +11,7 @@ import { Breadcrumb } from 'antd';
 ReactDOM.render(
   <React.StrictMode>
     
-    <Breadcrumb>
-    <Breadcrumb.Item>Home</Breadcrumb.Item>
-    <Breadcrumb.Item>
-      <a href="">Application Center</a>
-    </Breadcrumb.Item>
-    <Breadcrumb.Item>
-      <a href="">Application List</a>
-    </Breadcrumb.Item>
-    <Breadcrumb.Item>An Application</Breadcrumb.Item>
-  </Breadcrumb>
   <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
