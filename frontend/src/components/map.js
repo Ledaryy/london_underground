49b628dccc85d18b5e1ee869314edb0ad7e5e16c
Ledary <andrejkulishov@gmail.com>
2021-05-13 17:40:12 +0100

    Major map update, bounds, marker and auto zoom

--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -1,24 +1,30 @@
 function Map() {
-    
-var map = L.map('mapid').setView([0, 0], 0);
-L.tileLayer('static/map/{z}/{x}/{y}.png', {
-  continuousWorld: false,
-  noWrap: true,  
-  minZoom: 1,
-  maxZoom: 5,
-}).addTo(map);
+        
+    var southWest = L.latLng(85.0, -180.0)
+    var northEast = L.latLng(-85.0, 180.0)
+    var bounds = L.latLngBounds(southWest, northEast);
 
-var marker = L.marker([0, 0], {
-  draggable: true,
-}).addTo(map);
-marker.bindPopup('Marker').openPopup();
-marker.on('dragend', function(e) {
-  marker.getPopup().setContent(marker.getLatLng().toString()).openOn(map);
-});
+    var map = L.map('mapid').setView([0, 0], 0);
+    L.tileLayer('static/map/{z}/{x}/{y}.png', {
+    continuousWorld: false,
+    noWrap: true,  
+    minZoom: 0,
+    maxZoom: 5,
+    bounds: bounds
+    }).addTo(map);
+
+    map.flyTo([65.9, -21.7], 3); // Center of the map
+
+    var marker = L.marker([65.9, -21.7], {
+    draggable: true,
+    }).addTo(map);
+    marker.bindPopup('You can drag me to move around').openPopup();
+    marker.on('dragend', function(e) {
+    marker.getPopup().setContent(marker.getLatLng().toString()).openOn(map);
+    map.flyTo(marker.getLatLng(), 3);
+    });
+
 }
 
-
-
-    
 export default Map;
 
